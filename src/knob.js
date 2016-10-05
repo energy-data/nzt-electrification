536edd519b43618c0d2c6f663d8837eedba1ee5e
svexican <svexican@gmail.com>
2016-10-04 22:45:03 -0500

    knob icon and refactoring

--- src/knob.js
+++ src/knob.js
@@ -1,20 +1,20 @@
 define(['d3'], (d3) => {
-  var kc = d3.select('#knobs-container');
+  var kc = null;
 
-  var width  = 200;
-  var height = 230;
+  var svg = null;
 
-  var center = {
-    x: width / 2,
-    y: (width / 2) + 20
-  };
+  var width = 0;
+  var height = 0;
+
+  var center = null
 
   var pointer = {};
 
+  var knob0_radius = 0;
+  var knob1_radius = 0;
+
   var pi = Math.PI;
   var pi2 = 2 * pi;
-
-  var svg = null;
 
   var steps = 5;
 
@@ -22,9 +22,6 @@ define(['d3'], (d3) => {
 
   var min_angle = pi * (1/8);
   var max_angle = pi * (7/8);
-
-  var knob0_radius = width * (7/16);
-  var knob1_radius = width * (4/16);
 
   var range = [...Array(5)].map((_,i) => _u.l_scale(i, [0, steps-1], [min_angle, max_angle])).reverse();
 
@@ -116,6 +113,8 @@ define(['d3'], (d3) => {
 
     let knob0 = knobs.append('g').attr('id', 'knob0');
 
+    let knob1 = knobs.append('g').attr('id', 'knob1');
+
     let base0 = knob0.append('circle')
         .attr('id', 'knob0')
         .attr('r', knob0_radius)
@@ -124,38 +123,6 @@ define(['d3'], (d3) => {
         .attr('stroke', '#b8b8b8')
         .attr('stroke-width', '0.06em')
         .attr('fill', 'url(#gray)');
-
-    let t = range[_d.scenario['tier'] - 1];
-    let pc = polar_to_cartesian(knob0_radius - (width/13), -t);
-
-    let marker0 = knob0.append('g')
-        .attr('id', 'marker0');
-
-    marker0.append('circle')
-      .attr('r', width / 20)
-      .attr('cx', pc.x + center.x)
-      .attr('cy', pc.y + center.y)
-      .attr('fill', 'url(#gray_up)');
-
-    marker0.append('circle')
-      .attr('cx', pc.x + center.x)
-      .attr('cy', pc.y + center.y)
-      .attr('r', width / 70)
-      .attr('fill', '#7587A6');
-
-    marker0.call(
-      d3.drag()
-        .on('drag', function() {
-          rotate(this, '#marker0', knob0);
-        })
-        .on('start', () => { tier = _d.scenario['tier'] })
-        .on('end', () => {
-          if (_d.scenario['tier'] !== tier)
-            console.log("Changed to tier", _d.scenario['tier'] = tier);
-        })
-    );
-
-    let knob1 = knobs.append('g').attr('id', 'knob1');
 
     let base1 = knob1.append('circle')
         .attr('r', knob1_radius)
@@ -166,39 +133,89 @@ define(['d3'], (d3) => {
         .attr('fill', 'url(#gray)')
         .classed('nps', _d.scenario['diesel_p'] !== 'n');
 
-    let knob1_text = knob1.append('text');
+    // knob0 marker
+    {
+      let t = range[_d.scenario['tier'] - 1];
+      let pc = polar_to_cartesian(knob0_radius - (width/13), -t);
 
-    let toggle_nps = () => {
-      base1.classed('nps', !base1.classed('nps'));
+      let marker0 = knob0.append('g').attr('id', 'marker0');
 
-      if (base1.classed('nps')) {
-        knob1_text.text("HIGH");
-        base1.attr('fill', 'url(#gray_up)');
+      marker0.append('circle')
+        .attr('r', width / 20)
+        .attr('cx', pc.x + center.x)
+        .attr('cy', pc.y + center.y)
+        .attr('fill', 'url(#gray_up)');
 
-        t = 'n';
-      }
+      marker0.append('circle')
+        .attr('cx', pc.x + center.x)
+        .attr('cy', pc.y + center.y)
+        .attr('r', width / 70)
+        .attr('fill', '#7587A6');
 
-      else {
-        knob1_text.text("LOW");
-        base1.attr('fill', 'url(#gray)');
+      marker0.call(
+        d3.drag()
+          .on('drag', function() { rotate(this, '#marker0', knob0); })
+          .on('start', () => { tier = _d.scenario['tier'] })
+          .on('end', () => {
+            if (_d.scenario['tier'] !== tier)
+              console.log("Changed to tier", _d.scenario['tier'] = tier);
+          })
+      );
+    }
 
-        t = 'l';
-      }
+    // knob1 icon:
+    {
+      let knob1_icon = knob1.append('text')
+          .attr('id', 'diesel_p')
+          .attr('class', 'material-icons')
+          .text("local_gas_station");
 
-      let box = knob1_text.node().getBBox()
+      let box = knob1_icon.node().getBBox()
 
       let x = (center.x - (box['width']  / 2));
-      let y = (center.y + (box['height'] / 2)) - 5;
+      let y = (center.y + (box['height'] / 2)) - 15;
 
-      knob1_text
+      knob1_icon
         .attr('transform', `translate(${ x }, ${ y })`);
-    };
+    }
 
-    knob1
-      .on('mousedown', () => { toggle_nps() })
-      .on('mouseup',   () => { console.log("Changed to diesel price", _d.scenario['diesel_p'] = t) });
+    // knob1 text:
+    {
+      let knob1_text = knob1.append('text')
+          .attr('class', 'text');
 
-    toggle_nps();
+      var toggle_nps = () => {
+        base1.classed('nps', !base1.classed('nps'));
+
+        if (base1.classed('nps')) {
+          knob1_text.text("HIGH");
+          base1.attr('fill', 'url(#gray_up)');
+
+          t = 'n';
+        }
+
+        else {
+          knob1_text.text("LOW");
+          base1.attr('fill', 'url(#gray)');
+
+          t = 'l';
+        }
+
+        let box = knob1_text.node().getBBox()
+
+        let x = (center.x - (box['width']  / 2));
+        let y = (center.y + (box['height'] / 2)) + 12;
+
+        knob1_text
+          .attr('transform', `translate(${ x }, ${ y })`);
+      };
+
+      knob1
+        .on('mousedown', () => { toggle_nps() })
+        .on('mouseup',   () => { console.log("Changed to diesel price", _d.scenario['diesel_p'] = t) });
+
+      toggle_nps();
+    }
   };
 
   var draw_labels = () => {
@@ -217,7 +234,7 @@ define(['d3'], (d3) => {
     for (let i = 0; i < steps; i++) {
       let text = svg.append('text')
           .attr('dx', (48 * i)) // TODO: this 4_ is empirical...
-          .attr('dy', -width/21)
+          .attr('dy', -width/10)
           .attr('class', 'tier-label monospace')
 
           .on('click', () => {
@@ -234,10 +251,25 @@ define(['d3'], (d3) => {
   var clear = () => kc.empty();
 
   var init = () => {
+    kc = d3.select('#knobs-container');
+
+    let total_width = $('#controls')[0].clientWidth;
+
+    width  = total_width - 60;
+    height = total_width;
+
+    knob0_radius = width * (8/16);
+    knob1_radius = width * (5/16);
+
+    center = {
+      x: (total_width / 2),
+      y: (total_width / 2)
+    };
+
     clear();
 
     svg = kc.append('svg')
-      .attr('width', width)
+      .attr('width', total_width)
       .attr('height', height);
 
     do_defs();
